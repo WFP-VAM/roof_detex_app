8a5ff419297f4bea6c6a904d31ceb685e21e26f2
lorenzo.riches <lorenzo.riches@wfp.org>
2018-01-30 12:15:24 +0100

    indent problem

--- app.py
+++ app.py
@@ -85,5 +85,5 @@ if __name__ == '__main__':
 	graph = tf.get_default_graph()
 
 	# Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port)
