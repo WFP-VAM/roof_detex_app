690672fa4145831628873648d3fc33dd8d6f9797
lorenzo.riches <lorenzo.riches@wfp.org>
2018-02-08 17:26:59 +0100

    matplotlib shows output

--- app.py
+++ app.py
@@ -1,89 +1,59 @@
-import io
 import os
-from flask import Flask, request, g
+from flask import Flask, request
 from flask import send_file, render_template
 from PIL import Image
 import numpy as np
 import tensorflow as tf
 from tensorflow.python.keras.models import load_model
 from number_of_islands import Graph
-#import matplotlib.pyplot as plt
-#plt.use('Agg')
-
+import matplotlib.pyplot as plt
+import io
 app = Flask(__name__, instance_relative_config=True)
-
-
-def ml_predict(image):
-    with graph.as_default():
-        # Add a dimension for the batch
-        prediction = model.predict(image[None, :, :, :])
-    prediction = prediction.reshape((400,400, -1))
-    return prediction
-
 
 
 @app.route('/')
 def home():
-	return render_template('index.html')
-
+    return render_template('index.html')
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
     # Load image
     params = request.form
+
     img_rows, img_cols = int(params['row pixels']), int(params['column pixels'])
-    print(img_rows, img_cols)
+
     image = request.files['file']
     image = Image.open(image)
-    img = np.array(image).astype('float32')
 
-    # crop only to relevant pixels
-    img = img[:img_rows, :img_cols]
+    img = np.array(image).astype('float32')
 
     res = model.predict(img.reshape(1, img_rows, img_cols, 3))
     g = Graph(img_rows, img_cols, res.reshape(img_rows, img_cols))
 
+    # generate image with result
+    fig = plt.figure(figsize=(5, 5), dpi=300)
+    ax = fig.add_subplot(111)
+    ax.imshow(img.astype('uint8'))
+    ax.imshow(res.reshape(img_rows, img_cols)*100, alpha=0.3, cmap='gray')
+    ax.set_title('huts detected: {}'.format(g.countIslands()))
 
-    # Append transparency 4th channel to the 3 RGB image channels.
-    print('image array: ', np.array(image).shape)
-    print('result array: ', res.shape)
-    # plt.figure()
-    # plt.imshow(img)
-    # plt.imshow(res.reshape(img_rows, img_cols), alpha=0.6)
-    #
-    # byte_io = io.BytesIO()
-    #
-    # plt.savefig(byte_io)
-    # byte_io.seek(0)
-    # return send_file(byte_io, mimetype='image/png')
-    app_result = {'roofs': str(g.countIslands())}
-    print(str(g.countIslands()))
-    return render_template('result.html', roofs=app_result['roofs'])
-    # transparent_image = np.append(np.array(image), res.reshape(400, 400)*255., axis=-1)
-    # transparent_image = Image.fromarray(transparent_image)
-    # transparent_image = transparent_image.convert('RGB')
+    output = io.BytesIO()
+    plt.savefig(output, dpi=fig.dpi)
+    output.seek(0)
 
-
-    # Send back the result image to the client
-    # byte_io = io.BytesIO()
-    # transparent_image.save(byte_io, 'PNG')
-    # byte_io.seek(0)
-    # return send_file(byte_io, mimetype='image/png')
-
-# @app.route('/')
-# def homepage():
-#     return render_template('index.html')
+    return send_file(output, mimetype='image/png')
 
 
 if __name__ == '__main__':
-	
-	# Preload our model
-	print(("* Loading Keras model and Flask starting server..."
-        "please wait until server has fully started"))
-	model = load_model('model_ft2.h5', compile=False)
-	graph = tf.get_default_graph()
 
-	# Bind to PORT if defined, otherwise default to 5000.
-	port = int(os.environ.get('PORT', 5000))
-	app.run(host='0.0.0.0', port=port)
+    # Preload our model
+    print(("* Loading Keras model and Flask starting server..."
+            "please wait until server has fully started"))
+
+    model = load_model('model_ft2.h5', compile=False)
+    graph = tf.get_default_graph()
+
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
